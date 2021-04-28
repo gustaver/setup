ef82ddcee55d6bf7bc65453d98272caaec377057
Gustave Rousselet <gustaverousselet@Gustaves-Air.lan>
2021-04-28 13:13:22 +0200

    zsh-syntax-highlighting

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ code --install-extension ms-python.python && code --install-extension ms-vscode.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Powerlevel10k 
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
